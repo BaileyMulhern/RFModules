41ceffee202d7259ae04e115df16f1c198105051
Bailey <mulhern.15@osu.edu>
2019-02-01 00:14:44 -0500

    Began updating transmitter with led strip class

--- RFReceiver/RFReceiver.ino
+++ RFReceiver/RFReceiver.ino
@@ -2,19 +2,35 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+/* Gamma Correction Table */
+const uint8_t PROGMEM gammaTable[] = {
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+  2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+  90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+//Pin Definitions
 #define CE_PIN          9
 #define CSN_PIN         10
 #define MOSI_PIN        11
 #define CLOCK_PIN       13
-#define RED_LED_PIN     2  
-#define BLUE_LED_PIN    4
 #define STRIP_RED_PIN   3
 #define STRIP_GREEN_PIN 5
 #define STRIP_BLUE_PIN  6
 
-#define RED_LED   1
-#define BLUE_LED  2
-
+//Defining Message Bits
 #define RED_SHIFT   16
 #define GREEN_SHIFT 8
 #define BLUE_SHIFT  0
@@ -30,9 +46,6 @@ uint8_t blue;
 
 void setup() 
 {
-  pinMode(RED_LED_PIN, OUTPUT);
-  pinMode(BLUE_LED_PIN, OUTPUT);
-
   pinMode(STRIP_RED_PIN, OUTPUT);
   pinMode(STRIP_GREEN_PIN, OUTPUT);
   pinMode(STRIP_BLUE_PIN, OUTPUT);
@@ -56,9 +69,10 @@ void loop()
     //char text[32] = "";                 //Saving the incoming data
     //radio.read(&text, sizeof(text));    //Reading the data
     radio.read(&message, sizeof(message));    //Reading the data
-
     //You need this delay dont ask me why
     delay(5);
+
+    decode(message);
   }
 
   analogWrite(STRIP_RED_PIN, red);
--- RFTransmitter/RFTransmitter.ino
+++ RFTransmitter/RFTransmitter.ino
@@ -11,17 +11,81 @@
 #define GREEN_SHIFT 8
 #define BLUE_SHIFT  0
 
+#define LED_MAX 255
+
 RF24 radio(CE_PIN, CSN_PIN);       
 const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
 boolean button_one_state = 0;
 boolean button_two_state = 0;
 
-uint8_t message;
+uint32_t message;
+void encode(uint32_t *message);
+uint8_t g_red;
+uint8_t g_green;
+uint8_t g_blue;
+
+int count;
+int count_step;
+int count_max;
+int wait;
+
+
+
+class Timer {
+  private:
+    uint64_t wait;
+    uint64_t last_tic;
+    uint16_t count;           //Counter variable to keep track of where in the effect the task is
+    uint16_t count_max;       //Max number of counts before the counter rolls over
+    uint16_t count_step;      //Amount the counter increases each time its incremented
+    uint16_t count_overflow;  //Value that the counter overflows to 
+    
+  public:
+    Timer(uint64_t wait, uint16_t count, uint16_t count_max, uint16_t count_step, uint16_t count_overflow);
+    uint16_t tic(uint64_t curr_ms);
+};
+
+typedef enum {
+  OFF = 0,
+  IDLE = 1,
+  TEST = 2,
+}strip_state_t;
+
+class LedStrip {
+  private:
+    uint8_t address[6];
+    uint32_t packet;
+    strip_state_t state;
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint16_t count;
+    Timer *counter;
+
+    void off();
+    void idle();
+    void test();
+  public:
+    LedStrip(const uint8_t *address[6]);
+    ~LedStrip();
+    uint32_t getPacket();
+    void setState(strip_state_t state);
+    void update();
+};
 
 void setup() 
 {
-  pinMode(BUTTON_ONE_PIN, INPUT);
-  pinMode(BUTTON_TWO_PIN, INPUT);
+  //pinMode(BUTTON_ONE_PIN, INPUT);
+  //pinMode(BUTTON_TWO_PIN, INPUT);
+  
+  g_red = 0;
+  g_green = 0;
+  g_blue = 0;
+
+  count = 0;
+  count_step = 1;
+  count_max = 256;
+  wait = 50;
 
   Serial.begin(9600);
  
@@ -30,16 +94,121 @@ void setup()
   radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
   radio.stopListening();          //This sets the module as transmitter
 }
+
 void loop()
 {
   message = 0;
   
-  button_one_state = digitalRead(BUTTON_ONE_PIN);
-  button_two_state = digitalRead(BUTTON_TWO_PIN);
+  //button_one_state = digitalRead(BUTTON_ONE_PIN);
+  //button_two_state = digitalRead(BUTTON_TWO_PIN);
 
-  message |= (button_one_state * 0xFF) << RED_SHIFT;
-  message |= (button_two_state * 0xFF) << BLUE_SHIFT;
+  //message |= (uint32_t) button_one_state * 0xFF << RED_SHIFT;
+  //message |= (uint32_t) button_two_state * 0xFF << BLUE_SHIFT;
+
+  Wheel(count);
+
+  encode(&message);
+  Serial.println(message, HEX);
+
+  Serial.print(g_red, HEX);
+  Serial.print("\t");
+  Serial.print(g_green, HEX);
+  Serial.print("\t");
+  Serial.print(g_blue, HEX);
+  Serial.println();
 
   radio.write(&message, sizeof(message));  //Sending the message to receiver 
-  delay(5);
+
+  count += count_step;
+  if(count >= count_max)
+  {
+    count = 0;
+  }
+  delay(wait);
+}
+
+Timer::Timer(uint64_t wait, uint16_t count, uint16_t count_max, uint16_t count_step, uint16_t count_overflow)
+{
+  this->count = count;
+  this->count_max = count_max;
+  this->count_step = count_step;
+  this->count_overflow = count_overflow;
+  this->wait = millis();
+}
+    
+uint16_t Timer::tic(uint64_t curr_ms)
+{
+  if(this->count_step != 0 && curr_ms - this->last_tic >= this->wait)
+  {
+    this->last_tic = curr_ms;
+    this->count += this->count_step;
+    if(this->count >= this->count_max)
+    {
+      this->count = this->count_overflow;
+    }
+  }
+
+  return this->count;
+}
+
+LedStrip::LedStrip(const uint8_t *address[6])
+{
+  this->address = *address;
+  this->packet = 0;
+  this->red;
+  this->green;
+  this->blue;
+  this->count;
+  this->counter = new Timer(0,0,0,0,0);
+
+  setState(strip_state_t::OFF);
+}
+    
+LedStrip::~LedStrip
+{
+  if(counter != NULL)
+  {
+    delete counter;
+  }
+}
+
+uint32_t LedStrip::getPacket();
+
+void LedStrip::update();
+
+void encode(uint32_t *message)
+{
+  uint32_t temp = 0;
+  temp |= (uint32_t)g_red << RED_SHIFT;
+  temp |= (uint32_t)g_green << GREEN_SHIFT;
+  temp |= (uint32_t)g_blue << BLUE_SHIFT;
+  *message = temp;
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+void Wheel(uint8_t p) 
+{
+  p = 255 - p;
+  
+  if(p < 85) 
+  {
+    g_red = LED_MAX - p * 3;
+    g_green = 0;
+    g_blue = p * 3;
+  }
+  else if(p < 170) 
+  {
+    p -= 85;
+    g_red = 0;
+    g_green = p * 3;
+    g_blue = LED_MAX - p * 3;
+  }
+  else
+  {
+    p -= 170;
+    g_red = p * 3;
+    g_green = LED_MAX - p * 3;
+    g_blue = 0;
+  }
 }
