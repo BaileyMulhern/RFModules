37a9b19f2de4d1438b7aab5742f20f19e638bb77
Bailey <mulhern.15@osu.edu>
2019-04-26 17:16:46 -0400

    updating RF receiver with multiple commands

--- RFReceiver/RFReceiver.ino
+++ RFReceiver/RFReceiver.ino
@@ -1,90 +1,128 @@
+#include <EEPROM.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-/* Gamma Correction Table */
-const uint8_t PROGMEM gammaTable[] = {
-  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-  2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-  90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+#define LOOP_DELAY  10
 
 //Pin Definitions
 #define CE_PIN          9
 #define CSN_PIN         10
 #define MOSI_PIN        11
 #define CLOCK_PIN       13
-#define STRIP_RED_PIN   3
-#define STRIP_GREEN_PIN 5
-#define STRIP_BLUE_PIN  6
+#define RED_PIN         3
+#define GREEN_PIN       5
+#define BLUE_PIN        6
 
 //Defining Message Bits
 #define RED_SHIFT   16
 #define GREEN_SHIFT 8
 #define BLUE_SHIFT  0
 
-RF24 radio(CE_PIN, CSN_PIN);
-const byte address[6] = "00001";
+/* Serial Pipes */
+#define WRITING_PIPE    0
+#define READING_PIPE    1
 
-uint32_t message;
-void decode(uint32_t message);
-uint8_t red;
-uint8_t green;
-uint8_t blue;
+enum EepromAddress {
+    ADDRESS             = 0,
+    CURRENT_EFFECT      = 1,
+};
+
+typedef enum : uint32_t {
+    CONNECT             = 0,
+    SET_ADDRESS         = 1,
+    OFF                 = 2,
+    DEPENDENT_MODE      = 3,
+    INDEPENDENT_MODE    = 4,
+    RESET               = 5,
+}command_t;
+
+RF24 radio(CE_PIN, CSN_PIN);
+void initRF();
+void readRF();
+uint8_t node_index = 1;
+const uint8_t master_address[6] = "0Node";
+
+void updateEffect();
+bool independent = true;
+int current_effect = 0;
+uint8_t red = 0;
+uint8_t green = 0;
+uint8_t blue = 0;
 
 void setup() 
 {
-  pinMode(STRIP_RED_PIN, OUTPUT);
-  pinMode(STRIP_GREEN_PIN, OUTPUT);
-  pinMode(STRIP_BLUE_PIN, OUTPUT);
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
-  red = 0;
-  green = 0;
-  blue = 0;
+    current_effect = EEPROM.read(CURRENT_EFFECT);
 
-  Serial.begin(9600);
+    initRF();
 
-  radio.begin();
-  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
-  radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
-  radio.startListening();              //This sets the module as receiver
+    Serial.begin(9600);
 }
 void loop()
 {
-  if (radio.available())              //Looking for the data.
-  {
-    message = 0;
-    //char text[32] = "";                 //Saving the incoming data
-    //radio.read(&text, sizeof(text));    //Reading the data
-    radio.read(&message, sizeof(message));    //Reading the data
-    //You need this delay dont ask me why
-    delayMicroseconds(10);
+    readRF();
 
-    decode(message);
-  }
+    if(independent)
+    {
+        /* Update red, green, blue based on the current effect */
+    }
 
-  analogWrite(STRIP_RED_PIN, red);
-  analogWrite(STRIP_GREEN_PIN, green);
-  analogWrite(STRIP_BLUE_PIN, blue);
+    analogWrite(RED_PIN, red);
+    analogWrite(GREEN_PIN, green);
+    analogWrite(BLUE_PIN, blue);
 
-  delayMicroseconds(10);
+    delayMicroseconds(LOOP_DELAY);
 } 
 
-void decode(uint32_t message)
+void initRF()
+{
+    /* Start the radio */
+    radio.begin();
+
+    /* Get node index from eeprom */
+    node_index = EEPROM.read(ADDRESS);
+    /* Ensure the index is 1 - 5 */
+    if(0 > node_index || node_index > 5)
+    {
+        node_index = 1;
+    }
+    /* Set the address of the radio */
+    uint8_t address[6] = "#Node";
+    address[0] = node_index + '0';
+    radio.openReadingPipe(READING_PIPE, address);  
+
+    /* Open the writing pipe on 0 to master */
+    radio.openWritingPipe(master_address);
+
+    /* Set the distance from transmitter */
+    radio.setPALevel(RF24_PA_MIN); 
+    
+    /* Start listening */
+    radio.startListening();
+}
+
+void readRF()
+{
+    if(radio.available())
+    {
+        command_t command;
+        radio.read(&command, sizeof(command_t));    
+        delayMicroseconds(10);
+    }
+}
+
+void updateEffect()
+{
+    
+}
+
+/*void decode(uint32_t message)
 {
   red = (uint8_t) (message >> RED_SHIFT);
   green = (uint8_t) (message >> GREEN_SHIFT);
   blue = (uint8_t) (message >> BLUE_SHIFT);
-}
+}*/
